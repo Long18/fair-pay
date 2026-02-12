e4dd088903cf885197629fde71a2ee85aab109da
Thành Long <us.thanhlong18@gmail.com>
2026-02-12 15:29:31 +0700

    feat(ui): implement AdminUsers page with data table and role management

--- src/modules/admin/index.ts
+++ src/modules/admin/index.ts
@@ -3,3 +3,4 @@ export { useIsAdmin } from "./hooks/use-is-admin";
 export { AdminGuard } from "./components/AdminGuard";
 export { AdminLayout } from "./components/AdminLayout";
 export { AdminOverview } from "./pages/AdminOverview";
+export { AdminUsers } from "./pages/AdminUsers";
